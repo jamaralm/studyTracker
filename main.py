710331132b4d8192bccd65be377bf2720ba939d0
jamaralm <jamaral07@gmail.com>
2025-02-04 22:27:50 -0300

    EDIT AND REMOVE ADDED

--- main.py
+++ main.py
@@ -24,24 +24,64 @@ def addStudyTime(date, hours, minutes):
 
     data.append({'date': date, 'hours': hours, 'minutes': minutes})
     saveData(data)
-    print('Sucesfully registered the study time!! =D')
+    print('Sucesfully registered the study time!!g =D')
 
-def showTimeStudied():
+def showRecords():
     data = loadData(filePath)
     if not data:
         print('YOU DONT HAVE ANY TIME STUDIED :(')
 
+    print('##########################################')
     print('STUDY TRACKER:')
-    for record in data:
-        print(f"DATE: {record['date']} | TIME STUDIED: {record['hours']}h:{record['minutes']}m")
+    for index, record in enumerate(data):
+        print(f"{index + 1}. DATE: {record['date']} | TIME STUDIED: {record['hours']}h:{record['minutes']}m")
+
+def editRecord():
+    data = loadData(filePath)
+    if not data:
+        return
+
+    try:
+        index = int(input("TYPE THE NUMBER OF RECORD YOU WANT TO EDIT: ")) - 1
+        if 0 <= index < len(data):
+            new_hours = int(input("TYPE THE NEW AMOUNT OF HOURS STUDIED: "))
+            new_minutes = int(input("TYPE THE NEW AMOUNT OF MINUTES STUDIED: "))
+            data[index]["hours"] = new_hours
+            data[index]['minutes'] = new_minutes
+            saveData(data)
+            print("Registro editado com sucesso!\n")
+        else:
+            print("Índice inválido!\n")
+    except ValueError:
+        print("Entrada inválida!\n")
+
+def removeRecord():
+    data = loadData()
+    showRecords()
+    if not data:
+        return
+    try:
+        index = int(input("Digite o número do registro que deseja remover: ")) - 1
+        if 0 <= index < len(data):
+            del data[index]
+            saveData(data)
+            print("Registro removido com sucesso!\n")
+        else:
+            print("Índice inválido!\n")
+    except ValueError:
+        print("Entrada inválida!\n")
 
 # MAIN CODE
 
 def main():
     while True:
+        print('##########################################')
         print("1. ADD STUDY TIME")
         print("2. SHOW STUDY TIME")
-        print("3. LEAVE :(")
+        print("3. EDIT RECORD")
+        print("4. REMOVE RECORD")
+        print("5. LEAVE :(")
+        print('##########################################')
         choice = input("CHOOSE A OPTION: ")
         
         if choice == "1":
@@ -54,9 +94,13 @@ def main():
             except ValueError:
                 print("Entrada inválida! Certifique-se de digitar a data e as horas corretamente.\n")
         elif choice == "2":
-            showTimeStudied()
+            showRecords()
         elif choice == "3":
-            print("Saindo...")
+            editRecord()
+        elif choice == "4":
+            removeRecord()
+        elif choice == '5':
+            print('LEAVING... =(')
             break
         else:
             print("Opção inválida!\n")
